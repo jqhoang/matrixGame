f14bce1cf8f9ddee51930da054bfe7a588aa4867
jqhoang <johnquochoang@gmail.com>
2019-10-31 04:20:25 -0700

    changed join to resolve

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var routes = require('./routes/routes.js');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'index.html'))
+    res.sendFile(path.resolve(__dirname,'public', 'index.html'))
 })
 
 app.use(routes);
